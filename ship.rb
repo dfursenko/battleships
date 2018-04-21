de186ac598d1aa06d02f2a308fbcb50ad1c35f22
Dmitry Fursenko <dfursenko@gmail.com>
2018-04-22 02:12:41 +0700

    Строки в символы

--- ship.rb
+++ ship.rb
@@ -1,16 +1,16 @@
 class Ship
   attr_reader :type, :name, :length, :status
-  attr_accessor :coords
+  attr_accessor :coords, :displayed
 
   def initialize(type, name, number)
     @type = type
     @name = name
     @number = number
     case type
-      when 'battleship' then @length = 4
-      when 'crase' then @length = 3
-      when 'destroyer' then @length = 2
-      when 'boat' then @length = 1
+      when :battleship then @length = 4
+      when :crase then @length = 3
+      when :destroyer then @length = 2
+      when :boat then @length = 1
       else false
     end
     @status = :whole
