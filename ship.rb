3782cd55f1bf611a7d9163ad5d68971696f05b6b
Dmitry Fursenko <dfursenko@gmail.com>
2018-04-22 01:26:03 +0700

    Доработка класса Ship

--- ship.rb
+++ ship.rb
@@ -1,3 +1,21 @@
 class Ship
+  attr_reader :type, :name, :length, :status
+  attr_accessor :coords
+
+  def initialize(type, name, number)
+    @type = type
+    @name = name
+    @number = number
+    case type
+      when 'battleship' then @length = 4
+      when 'crase' then @length = 3
+      when 'destroyer' then @length = 2
+      when 'boat' then @length = 1
+      else false
+    end
+    @status = :whole
+    @coords = []
+    @displayed = false
+  end
 
 end
