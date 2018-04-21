af65f949ca9ef606c8cd850a5f44b97a92b9e243
Dmitry Fursenko <dfursenko@gmail.com>
2018-04-22 01:26:39 +0700

    Доработка класса Fleet

--- fleet.rb
+++ fleet.rb
@@ -4,28 +4,17 @@ class Fleet
   attr_reader :ships
 
   def initialize
-    ship_a1 = Ship.new('battleship')
-    ship_b1 = Ship.new('crase')
-    ship_b2 = Ship.new('crase')
-    ship_c1 = Ship.new('destroyer')
-    ship_c2 = Ship.new('destroyer')
-    ship_c3 = Ship.new('destroyer')
-    ship_d1 = Ship.new('boat')
-    ship_d2 = Ship.new('boat')
-    ship_d3 = Ship.new('boat')
-    ship_d4 = Ship.new('boat')
-
-    @ships = [
-        {a1: ship_a1},
-        {b1: ship_b1},
-        {b2: ship_b2},
-        {c1: ship_c1},
-        {c2: ship_c2},
-        {c3: ship_c3},
-        {d1: ship_d1},
-        {d2: ship_d2},
-        {d3: ship_d3},
-        {d4: ship_d4},
-    ]
+    @ships = {
+        a_1: Ship.new('battleship', :a1, 1),
+        b_1: Ship.new('crase', :b1, 1),
+        b_2: Ship.new('crase', :b2, 2),
+        c_1: Ship.new('destroyer', :c1, 1),
+        c_2: Ship.new('destroyer', :c2, 2),
+        c_3: Ship.new('destroyer', :c3, 3),
+        d_1: Ship.new('boat', :d1, 1),
+        d_2: Ship.new('boat', :d2, 2),
+        d_3: Ship.new('boat', :d3, 3),
+        d_4: Ship.new('boat', :d4, 4)
+    }
   end
 end
