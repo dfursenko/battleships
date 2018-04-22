4559fcfc0eef9998d9b39d6106571bc72fe81e5a
Dmitry Fursenko <dfursenko@gmail.com>
2018-04-22 23:37:40 +0700

    Дополнен класс Coord

--- coord.rb
+++ coord.rb
@@ -1,17 +1,18 @@
 class Coord
   def initialize(x,y)
-
+    @x = x
+    @y = y
   end
 
   def ship_name(name)
-
+    @ship_name = name
   end
 
   def ship_length(length)
-
+    @ship_length = length
   end
 
   def check_ship_length(ship)
-
+    ship.length == ship.coords.length
   end
 end
