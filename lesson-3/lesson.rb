073d3cec889e10a222baf60342f6a9069048fdf7
Dmitry Fursenko <dfursenko@gmail.com>
2018-04-25 22:14:10 +0700

    Небольшие доработки

--- lesson-3/lesson.rb
+++ lesson-3/lesson.rb
@@ -14,33 +14,39 @@
   end
 
   # стандартный вызов
-  my_loop {|n| puts n}
+  my_loop {|n| puts n} # => Начало 1 2 3 Завершение
 
   # вызов до нужного числа
-  my_loop(10) {|n| puts n}
+  my_loop(10) {|n| puts n} # => Начало 1 2 3 4 5 6 7 8 9 10 Завершение
 
   # вызов без блока
-  my_loop
+  my_loop # => Начало Завершение
 
 
-# 2. Доработать Ship.how_many_ships(length)
+# 2. Доработать Ship.how_many_ships(length)...
 
   # Сделано:
   # - проверка корректности вводимых данных
   # - использован case/when
   # - использован условный модификатор
-  def Ship.how_many_ships(length)
-    raise self.wrong_coords_message unless (1..4) === length
+  class Ship
+    def Ship.how_many_ships(length)
+      raise self.wrong_coords_message unless (1..4) === length
 
-    case length
-      when 1 then 4
-      when 2 then 3
-      when 3 then 4
-      when 4 then 1
-      else false
+      puts count =
+        case length
+          when 1 then 4
+          when 2 then 3
+          when 3 then 2
+          when 4 then 1
+          else false
+        end
+    end
+
+    def Ship.wrong_coords_message
+      'Введено некорректное количество координат кораблика. Пожалуйста, введите число от 1 до 4'
     end
   end
 
-  def Ship.wrong_coords_message
-    'Введено некорректное количество координат кораблика. Пожалуйста, введите число от 1 до 4'
-  end
+  Ship.how_many_ships(3) # => 2
+  Ship.how_many_ships(7) # => 'Введено некорректное количество координат кораблика...'
