23eac8a45cb4f885e6a1d58085ffd1afd0dcb700
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-03 03:08:41 +0700

    Правки

--- lesson-4/exercise_4.rb
+++ lesson-4/exercise_4.rb
@@ -5,9 +5,7 @@ require './classes/worker'
 # 4. К предыдущей задаче добавьте метод, который проверял бы наличие введенной
 # зарплаты и выводил бы сообщение об ошибке, если зарплата не была указана.
 
-print 'Введите имя сотрудника и его зарплату (имя зарплата): '
-name, salary = gets.chomp.split(' ')
-worker = Worker.new(name, salary)
+worker = Worker.get
 
 puts 'Сотрудник ' + worker.name + ' в этом месяце получит ' +
          worker.salary.bonus_int_s_with_spaces + ' ' + worker.salary.formatted(:bonus_int) + ', ' +
