9767d76fbf9feb6efec4c89aa0d03e982dca2dc5
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-05 00:57:36 +0700

    Оформление

--- lesson-5/exercise_3.rb
+++ lesson-5/exercise_3.rb
@@ -6,6 +6,10 @@ require_relative 'classes/ship'
 #   а в качестве значения количество корабликов с этим количеством палуб. При создании нового кораблика нужно
 #   вносить соответствующие изменения в хэш.
 
+# Сделано:
+# - Обеспечивает хранение данных о количестве кораблей по их типам
+# - Предотвращает создание нового корабля, если превышено количество типов данного корабля
+
 boat_1 = Ship.new(:boat)              # boat 1
 p Ship.ships                                  # => @@ships = {4=>1, 3=>0, 2=>0, 1=>0}
 
@@ -18,7 +22,7 @@ p Ship.ships                                  # => @@ships = {4=>3, 3=>0, 2=>0, 
 boat_4 = Ship.new(:boat)              # boat 4
 p Ship.ships                                  # => @@ships = {4=>4, 3=>0, 2=>0, 1=>0}
 
-boat_5 = Ship.new(:boat)              # boat 5 => Катер не создан. Количество катеров не должно превышать 4.
+boat_5 = Ship.new(:boat)              # boat 5 => Катер не создан, их количество не должно превышать 4.
 p Ship.ships                                  # => @@ships = {4=>4, 3=>0, 2=>0, 1=>0}
 
 destroyer_1 = Ship.new(:destroyer)    # destroyer 1
@@ -30,7 +34,7 @@ p Ship.ships                                  # => @@ships = {4=>4, 3=>2, 2=>0, 
 destroyer_3 = Ship.new(:destroyer)    # destroyer 3
 p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>0, 1=>0}
 
-destroyer_4 = Ship.new(:destroyer)    # destroyer 4 => > Эсминец не создан. Количество эсминцев не должно превышать 3.
+destroyer_4 = Ship.new(:destroyer)    # destroyer 4 => > Эсминец не создан, их количество не должно превышать 3.
 p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>0, 1=>0}
 
 crase_1 = Ship.new(:crase)            # crase 1
@@ -39,13 +43,13 @@ p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>1, 
 crase_2 = Ship.new(:crase)            # crase 2
 p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>2, 1=>0}
 
-crase_3 = Ship.new(:crase)            # crase 3 => Крейсер не создан. Количество крейсеров не должно превышать 2.
+crase_3 = Ship.new(:crase)            # crase 3 => Крейсер не создан, их количество не должно превышать 2.
 p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>2, 1=>0}
 
 battleship_1 = Ship.new(:battleship)    # battleship 1
 p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>2, 1=>1}
 
-battleship_2 = Ship.new(:battleship)    # battleship 2 => Линкор не создан. Количество линкоров не должно превышать 1.
+battleship_2 = Ship.new(:battleship)    # battleship 2 => Линкор не создан, их количество не должно превышать 1.
 p Ship.ships                                  # => @@ships = {4=>4, 3=>3, 2=>2, 1=>1}
 
 puts "Всего построено кораблей: #{Ship.total_ships}"
