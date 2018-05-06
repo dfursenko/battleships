7fb57ca9534cddad8e6789f9bd5f0dfd9d64add8
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-06 16:24:42 +0700

    ДЗ 5. Задание 5. Завершено.

---
+++ lesson-5/exercise_5.rb
@@ -0,0 +1,12 @@
+require_relative 'classes/ship'
+
+# ДОМАШНЕЕ ЗАДАНИЕ. Урок 5. Задание 5.
+#
+# 5. Создать константы, которые содержали бы допустимое количество корабликов в целом и отдельно
+#   для каждого количества палуб.
+
+puts Ship::MAX_SHIPS              # => 10
+puts Ship::BOAT_QUANTITY          # => 4
+puts Ship::DESTROYER_QUANTITY     # => 3
+puts Ship::CRASE_QUANTITY         # => 2
+puts Ship::BATTLESHIP_QUANTITY    # => 1
