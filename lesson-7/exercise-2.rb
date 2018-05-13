d3fee34bf0b711f8d5eb123495e32aeaf0a93da6
Dmitry Fursenko <dfursenko@gmail.com>
2018-05-13 14:03:22 +0700

    ДЗ 7. Задание 2. Завершено.

---
+++ lesson-7/exercise-2.rb
@@ -0,0 +1,27 @@
+require_relative './classes/study_group'
+
+# ДОМАШНЕЕ ЗАДАНИЕ 7. Задание 1.
+
+# 2. Найти первого в списке посещающих кружок программирования школьника, чья фамилия начинается на “А”.
+#
+# Сделано:
+#   - Реализовано тремя способами:
+#     1. Методом .select
+#     2. Методом .find
+#     3. Методом .grep
+
+students = %w[Иванов Петров Сидоров Алексеева Казанцев Антропов Анисимова Кузнецов Соловьев Кошкина]
+group = StudyGroup.new(students)
+
+# Вариант 1: .select
+puts group.select {|student| student if student.split('').first == 'А' }.first
+
+# Вариант 2: .find
+puts group.find {|student| student.split('').first == 'А' }
+
+# Вариант 3: .grep
+puts group.grep(/^А/).first
+
+
+# РЕЗУЛЬТАТ:
+# => Алексеева
